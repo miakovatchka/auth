ff15400cf5805d5b2c72a4c0862c547dadbc25bf
Maria Kovatchka <maria.kovatchka@kellogg.northwestern.edu>
2024-02-20 16:16:48 +0000

    Class

--- scripts/encryption.rb
+++ scripts/encryption.rb
@@ -4,5 +4,9 @@
 
 # TODO:
 # 1. encrypt a secret and puts encrypted string
+secret = "123"
+result = BCrypt::Password.create(secret)
+puts result # BCrypt encrypted value for 123
 # 2. prepare encrypted string for testing
+puts BCrypt::Password.new(result) == secret
 # 3. test secret against prepared encrypted string
